a46a18f1ad288825439e8d1ca4802b985dc1669a
mr-mukhtar <mukhtarmohd517@gmail.com>
2023-08-17 11:20:17 +0530

    Added filter cpmponent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpenseFilter';
 
 const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState('2020')
+  const filterChangeHandler = selectedYear =>{
+    setFilterYear(selectedYear);
+  }
   return (
-    <div>
+    <div className='expenses'>
+      <ExpensesFilter selected={filterYear} onChange={filterChangeHandler}/>
       {props.expenses.map(expense => (
         <ExpenseItem
           key={expense.id}
